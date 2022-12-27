45b226acf3f5d77543c0a1c01d0b1adf30eb0a46
Wicus <wicus@mecalc.co.za>
2022-12-27 10:30:07 +0200

    Initial options and keymaps

--- init.lua
+++ init.lua
@@ -100,12 +100,17 @@ vim.o.hlsearch = false
 
 -- Make line numbers default
 vim.wo.number = true
+vim.o.relativenumber = true
 
 -- Enable mouse mode
 vim.o.mouse = "a"
 
 -- Enable break indent
 vim.o.breakindent = true
+
+-- Disable backup files
+vim.o.backup = false
+vim.o.swapfile = false
 
 -- Save undo history
 vim.o.undofile = true
@@ -115,7 +120,7 @@ vim.o.ignorecase = true
 vim.o.smartcase = true
 
 -- Decrease update time
-vim.o.updatetime = 250
+vim.o.updatetime = 50
 vim.wo.signcolumn = "yes"
 
 -- Set colorscheme
@@ -125,12 +130,18 @@ vim.cmd.colorscheme("onedark")
 -- Set completeopt to have a better completion experience
 vim.o.completeopt = "menuone,noselect"
 
+-- Highlight current linenumber
+vim.o.cursorline = true
+
+-- Color column
+vim.o.colorcolumn = "80"
+
 -- [[ Basic Keymaps ]]
 -- Set <space> as the leader key
 -- See `:help mapleader`
 --  NOTE: Must happen before plugins are required (otherwise wrong leader will be used)
 vim.g.mapleader = " "
-vim.g.maplocalleader = " "
+vim.g.maplocalleader = ","
 
 -- Keymaps for better default experience
 -- See `:help vim.keymap.set()`
@@ -140,12 +151,43 @@ vim.keymap.set({ "n", "v" }, "<Space>", "<Nop>", { silent = true })
 vim.keymap.set("n", "k", "v:count == 0 ? \"gk\" : \"k\"", { expr = true, silent = true })
 vim.keymap.set("n", "j", "v:count == 0 ? \"gj\" : \"j\"", { expr = true, silent = true })
 
+-- File explorer
+vim.keymap.set("n", "<leader>ft", vim.cmd.Ex)
+
+-- Remapping for better yank and paste
+vim.keymap.set("x", "<leader>p", "\"_dP")
+vim.keymap.set("n", "<leader>y", "\"+y")
+vim.keymap.set("v", "<leader>y", "\"+y")
+vim.keymap.set("n", "<leader>Y", "\"+Y")
+vim.keymap.set("n", "<leader>d", "\"_d")
+vim.keymap.set("v", "<leader>d", "\"_d")
+
+-- Move lines up and down with J and K
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+
+-- Keeps cursor on the same spot on K
+vim.keymap.set("n", "J", "mzJ`z")
+
+-- Keep centered while scrolling
+vim.keymap.set("n", "<C-d>", "<C-d>zz")
+vim.keymap.set("n", "<C-u>", "<C-u>zz")
+vim.keymap.set("n", "n", "nzzzv")
+vim.keymap.set("n", "N", "Nzzzv")
+
+-- Buffer commands
+vim.keymap.set("n", "<leader>bd", vim.cmd.bdelete, { desc = "[B]uffer [D]elete" })
+vim.keymap.set("n", "<leader>bp", vim.cmd.bprevious, { desc = "[B]uffer [P]revious" })
+vim.keymap.set("n", "<leader>bn", vim.cmd.bnext, { desc = "[B]uffer [N]ext" })
+
 -- [[ Highlight on yank ]]
 -- See `:help vim.highlight.on_yank()`
 local highlight_group = vim.api.nvim_create_augroup("YankHighlight", { clear = true })
 vim.api.nvim_create_autocmd("TextYankPost", {
   callback = function()
-    vim.highlight.on_yank()
+    vim.highlight.on_yank({
+      timeout = 40,
+    })
   end,
   group = highlight_group,
   pattern = "*",
@@ -190,8 +232,9 @@ require("telescope").setup {
   defaults = {
     mappings = {
       i = {
-        ["<C-u>"] = false,
-        ["<C-d>"] = false,
+        ["<C-j>"] = require("telescope.actions").move_selection_next,
+        ["<C-k>"] = require("telescope.actions").move_selection_previous,
+        ["<esc>"] = require("telescope.actions").close,
       },
     },
   },
@@ -201,37 +244,51 @@ require("telescope").setup {
 pcall(require("telescope").load_extension, "fzf")
 
 -- See `:help telescope.builtin`
-vim.keymap.set("n", "<leader>?", require("telescope.builtin").oldfiles, { desc = "[?] Find recently opened files" })
-vim.keymap.set("n", "<leader><space>", require("telescope.builtin").buffers, { desc = "[ ] Find existing buffers" })
-vim.keymap.set("n", "<leader>/", function()
+vim.keymap.set("n", "<leader>fr", require("telescope.builtin").oldfiles,
+  { desc = "[F]ile [R]ecent: Find recently opened files" })
+vim.keymap.set("n", "<leader>bb", require("telescope.builtin").buffers,
+  { desc = "[B]uffers [B]uffers: Find existing buffers" })
+vim.keymap.set("n", "<leader>ss", function()
   -- You can pass additional configuration to telescope to change theme, layout, etc.
-  require("telescope.builtin").current_buffer_fuzzy_find(require("telescope.themes").get_dropdown {
+  require("telescope.builtin").current_buffer_fuzzy_find(require("telescope.themes").get_dropdown({
     winblend = 10,
     previewer = false,
-  })
-end, { desc = "[/] Fuzzily search in current buffer]" })
+  }))
+end, { desc = "[S]earch [S]earch: Fuzzily search in current buffer" })
 
-vim.keymap.set("n", "<leader>sf", require("telescope.builtin").find_files, { desc = "[S]earch [F]iles" })
-vim.keymap.set("n", "<leader>sh", require("telescope.builtin").help_tags, { desc = "[S]earch [H]elp" })
-vim.keymap.set("n", "<leader>sw", require("telescope.builtin").grep_string, { desc = "[S]earch current [W]ord" })
-vim.keymap.set("n", "<leader>sg", require("telescope.builtin").live_grep, { desc = "[S]earch by [G]rep" })
-vim.keymap.set("n", "<leader>sd", require("telescope.builtin").diagnostics, { desc = "[S]earch [D]iagnostics" })
+vim.keymap.set("n", "<leader>/", require("telescope.builtin").live_grep, { desc = "[/]: Search in project" })
+vim.keymap.set("n", "<leader>*", require("telescope.builtin").grep_string,
+  { desc = "[*]: Search current word in project" })
+vim.keymap.set("n", "<leader>pf", require("telescope.builtin").find_files, { desc = "[P]roject [F]iles" })
+vim.keymap.set("n", "<leader><space>", require("telescope.builtin").help_tags, { desc = "[ ]: Open neovim command help" })
+
+
+-- Diagnostic keymaps
+vim.keymap.set("n", "<leader>el", require("telescope.builtin").diagnostics, { desc = "[E]rror [L]ist [T]elescope" })
+vim.keymap.set("n", "<leader>ee", vim.diagnostic.open_float, { desc = "[E]rror [L]ist [.]: List error under cursor" })
+vim.keymap.set("n", "<leader>eq", vim.diagnostic.setloclist, { desc = "[E]rror [L]ist [Q]uickfix" })
+
+vim.keymap.set("n", "[e", vim.diagnostic.goto_prev)
+vim.keymap.set("n", "<leader>ep", vim.diagnostic.goto_prev, { desc = "[E]rror [P]revious" })
+
+vim.keymap.set("n", "]e", vim.diagnostic.goto_next)
+vim.keymap.set("n", "<leader>en", vim.diagnostic.goto_next, { desc = "[E]rror [N]ext" })
 
 -- [[ Configure Treesitter ]]
 -- See `:help nvim-treesitter`
 require("nvim-treesitter.configs").setup {
   -- Add languages to be installed here that you want installed for treesitter
-  ensure_installed = { "c", "cpp", "go", "lua", "python", "rust", "typescript", "help" },
+  ensure_installed = { "c", "cpp", "lua", "python", "typescript", "help" },
 
   highlight = { enable = true },
   indent = { enable = true, disable = { "python" } },
   incremental_selection = {
     enable = true,
     keymaps = {
-      init_selection = "<c-space>",
-      node_incremental = "<c-space>",
-      scope_incremental = "<c-s>",
-      node_decremental = "<c-backspace>",
+      init_selection = "<leader>v",
+      node_incremental = "v",
+      node_decremental = "V",
+      scope_incremental = "<C-s>",
     },
   },
   textobjects = {
@@ -280,11 +337,6 @@ require("nvim-treesitter.configs").setup {
   },
 }
 
--- Diagnostic keymaps
-vim.keymap.set("n", "[d", vim.diagnostic.goto_prev)
-vim.keymap.set("n", "]d", vim.diagnostic.goto_next)
-vim.keymap.set("n", "<leader>e", vim.diagnostic.open_float)
-vim.keymap.set("n", "<leader>q", vim.diagnostic.setloclist)
 
 -- LSP settings.
 --  This function gets run when an LSP connects to a particular buffer.
@@ -303,27 +355,39 @@ local on_attach = function(_, bufnr)
     vim.keymap.set("n", keys, func, { buffer = bufnr, desc = desc })
   end
 
-  nmap("<leader>rn", vim.lsp.buf.rename, "[R]e[n]ame")
-  nmap("<leader>ca", vim.lsp.buf.code_action, "[C]ode [A]ction")
+  nmap("<leader>br", vim.lsp.buf.rename, "[B]uffer [R]ename")
+
+  nmap("<leader>sj", require("telescope.builtin").lsp_document_symbols, "[S]ymbols [J]ump: Document symbols")
+  nmap("<leader>sp", require("telescope.builtin").lsp_dynamic_workspace_symbols, "[S]ymbols [P]roject: Workspace symbols")
+
+  nmap("<C-.>", vim.lsp.buf.code_action, "[C-.] Code Action")
+  nmap("<localleader>r.", vim.lsp.buf.code_action, "[R]efactor: Code Actions")
 
   nmap("gd", vim.lsp.buf.definition, "[G]oto [D]efinition")
+  nmap("<localleader>gd", vim.lsp.buf.definition, "[G]oto [D]efinition")
+
   nmap("gr", require("telescope.builtin").lsp_references, "[G]oto [R]eferences")
+  nmap("<localleader>gr", require("telescope.builtin").lsp_references, "[G]oto [R]eferences")
+
   nmap("gI", vim.lsp.buf.implementation, "[G]oto [I]mplementation")
-  nmap("<leader>D", vim.lsp.buf.type_definition, "Type [D]efinition")
-  nmap("<leader>ds", require("telescope.builtin").lsp_document_symbols, "[D]ocument [S]ymbols")
-  nmap("<leader>ws", require("telescope.builtin").lsp_dynamic_workspace_symbols, "[W]orkspace [S]ymbols")
+  nmap("<localleader>gi", vim.lsp.buf.implementation, "[G]oto [I]mplementation")
+
+  nmap("<localleader>gt", vim.lsp.buf.type_definition, "[G]oto [T]ype definition")
+
 
   -- See `:help K` for why this keymap
-  nmap("K", vim.lsp.buf.hover, "Hover Documentation")
-  nmap("<C-k>", vim.lsp.buf.signature_help, "Signature Documentation")
+  nmap("K", vim.lsp.buf.hover, "[K]: Hover Documentation")
+  nmap("gh", vim.lsp.buf.signature_help, "[G]oto [H]elp: Signature Documentation")
 
   -- Lesser used LSP functionality
   nmap("gD", vim.lsp.buf.declaration, "[G]oto [D]eclaration")
-  nmap("<leader>wa", vim.lsp.buf.add_workspace_folder, "[W]orkspace [A]dd Folder")
-  nmap("<leader>wr", vim.lsp.buf.remove_workspace_folder, "[W]orkspace [R]emove Folder")
-  nmap("<leader>wl", function()
+  nmap("<localleader>gD", vim.lsp.buf.declaration, "[G]oto [D]eclaration")
+
+  nmap("<leader>pa", vim.lsp.buf.add_workspace_folder, "[P]roject [A]dd: Workspace add folder")
+  nmap("<leader>pd", vim.lsp.buf.remove_workspace_folder, "[P]roject [x] delete folder: Workspace remove folder")
+  nmap("<leader>pl", function()
     print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
-  end, "[W]orkspace [L]ist Folders")
+  end, "[P]roject [L]ist folders: Workspace list folders")
 
   -- Create a command `:Format` local to the LSP buffer
   vim.api.nvim_buf_create_user_command(bufnr, "Format", function(_)
@@ -393,13 +457,17 @@ cmp.setup {
   },
   mapping = cmp.mapping.preset.insert {
     ["<C-d>"] = cmp.mapping.scroll_docs(-4),
-    ["<C-f>"] = cmp.mapping.scroll_docs(4),
-    ["<C-Space>"] = cmp.mapping.complete(),
-    ["<CR>"] = cmp.mapping.confirm {
+    ["<C-u>"] = cmp.mapping.scroll_docs(4),
+    ["<C-y>"] = cmp.mapping.complete({}),
+    ["<C-l>"] = cmp.mapping.confirm({
       behavior = cmp.ConfirmBehavior.Replace,
       select = true,
-    },
-    ["<Tab>"] = cmp.mapping(function(fallback)
+    }),
+    ["<CR>"] = cmp.mapping.confirm({
+      behavior = cmp.ConfirmBehavior.Replace,
+      select = true,
+    }),
+    ["<C-j>"] = cmp.mapping(function(fallback)
       if cmp.visible() then
         cmp.select_next_item()
       elseif luasnip.expand_or_jumpable() then
@@ -408,7 +476,7 @@ cmp.setup {
         fallback()
       end
     end, { "i", "s" }),
-    ["<S-Tab>"] = cmp.mapping(function(fallback)
+    ["<C-k>"] = cmp.mapping(function(fallback)
       if cmp.visible() then
         cmp.select_prev_item()
       elseif luasnip.jumpable(-1) then
