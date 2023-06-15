82a937d260da8fa0f46c2a36529ab1802e704024
Wicus <wicus@mecalc.co.za>
2023-06-15 11:07:28 +0200

    omnisharp fixes

--- init.lua
+++ init.lua
@@ -156,7 +156,8 @@ vim.api.nvim_create_autocmd("BufWritePost", {
 -- disable netrw at the very start of your init.lua (strongly advised)
 vim.g.loaded_netrw = 1
 vim.g.loaded_netrwPlugin = 1
-vim.g.copilot_filetypes = { TelescopePrompt = false }
+
+vim.g.copilot_filetypes = { TelescopePrompt = false, text = false }
 
 -- Sane defaults for tabs and spaces
 vim.opt.tabstop = 4
@@ -809,82 +810,24 @@ local on_attach = function(client, bufnr)
 		vim.lsp.buf.format()
 	end, { desc = "Format current buffer with LSP" })
 
-	-- Hack for omnisharp
+	-- Omnisharp specific settings
 	if client.name == "omnisharp" then
-		client.server_capabilities.semanticTokensProvider = {
-			full = vim.empty_dict(),
-			legend = {
-				tokenModifiers = { "static_symbol" },
-				tokenTypes = {
-					"comment",
-					"excluded_code",
-					"identifier",
-					"keyword",
-					"keyword_control",
-					"number",
-					"operator",
-					"operator_overloaded",
-					"preprocessor_keyword",
-					"string",
-					"whitespace",
-					"text",
-					"static_symbol",
-					"preprocessor_text",
-					"punctuation",
-					"string_verbatim",
-					"string_escape_character",
-					"class_name",
-					"delegate_name",
-					"enum_name",
-					"interface_name",
-					"module_name",
-					"struct_name",
-					"type_parameter_name",
-					"field_name",
-					"enum_member_name",
-					"constant_name",
-					"local_name",
-					"parameter_name",
-					"method_name",
-					"extension_method_name",
-					"property_name",
-					"event_name",
-					"namespace_name",
-					"label_name",
-					"xml_doc_comment_attribute_name",
-					"xml_doc_comment_attribute_quotes",
-					"xml_doc_comment_attribute_value",
-					"xml_doc_comment_cdata_section",
-					"xml_doc_comment_comment",
-					"xml_doc_comment_delimiter",
-					"xml_doc_comment_entity_reference",
-					"xml_doc_comment_name",
-					"xml_doc_comment_processing_instruction",
-					"xml_doc_comment_text",
-					"xml_literal_attribute_name",
-					"xml_literal_attribute_quotes",
-					"xml_literal_attribute_value",
-					"xml_literal_cdata_section",
-					"xml_literal_comment",
-					"xml_literal_delimiter",
-					"xml_literal_embedded_expression",
-					"xml_literal_entity_reference",
-					"xml_literal_name",
-					"xml_literal_processing_instruction",
-					"xml_literal_text",
-					"regex_comment",
-					"regex_character_class",
-					"regex_anchor",
-					"regex_quantifier",
-					"regex_grouping",
-					"regex_alternation",
-					"regex_text",
-					"regex_self_escaped_character",
-					"regex_other_escape",
-				},
-			},
-			range = true,
-		}
+		nmap("gd", require("omnisharp_extended").telescope_lsp_definitions, "[G]oto [D]efinition")
+		nmap("<localleader>gd", require("omnisharp_extended").telescope_lsp_definitions, "[G]oto [D]efinition")
+
+		-- Omnisharp Semantic tokens do not conform to the LSP specification
+		-- For more details: https://github.com/OmniSharp/omnisharp-roslyn/issues/2483#issuecomment-1546721190
+		local function toSnakeCase(str)
+			return string.gsub(str, "%s*[- ]%s*", "_")
+		end
+		local tokenModifiers = client.server_capabilities.semanticTokensProvider.legend.tokenModifiers
+		for i, v in ipairs(tokenModifiers) do
+			tokenModifiers[i] = toSnakeCase(v)
+		end
+		local tokenTypes = client.server_capabilities.semanticTokensProvider.legend.tokenTypes
+		for i, v in ipairs(tokenTypes) do
+			tokenTypes[i] = toSnakeCase(v)
+		end
 	end
 end
 
