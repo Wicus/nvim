9f33e2febfde34ac8060160094f1c0075ca2fb1a
Wicus <wicus@mecalc.co.za>
2024-01-09 22:00:13 +0200

    Fix Tab for luasnip

--- lua/plugins/nvim-cmp.lua
+++ lua/plugins/nvim-cmp.lua
@@ -87,20 +87,20 @@ return {
 					select = true,
 				}),
 
-				-- ["<Tab>"] = cmp.mapping(function(fallback)
-				-- 	if luasnip.locally_jumpable() then
-				-- 		luasnip.jump(1)
-				-- 	else
-				-- 		fallback()
-				-- 	end
-				-- end, { "i", "s" }),
-				-- ["<S-Tab>"] = cmp.mapping(function(fallback)
-				-- 	if luasnip.locally_jumpable(-1) then
-				-- 		luasnip.jump(-1)
-				-- 	else
-				-- 		fallback()
-				-- 	end
-				-- end, { "i", "s" }),
+				["<Tab>"] = cmp.mapping(function(fallback)
+					if luasnip.expand_or_jumpable() then
+						luasnip.expand_or_jump()
+					else
+						fallback()
+					end
+				end, { "i", "s" }),
+				["<S-Tab>"] = cmp.mapping(function(fallback)
+					if luasnip.jumpable(-1) then
+						luasnip.jump(-1)
+					else
+						fallback()
+					end
+				end, { "i", "s" }),
 			}),
 			sources = {
 				{ name = "luasnip" },
