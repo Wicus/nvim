7d41087ace00ffeb498cbb50c2fbc009adc630fd
Wicus <wicus@mecalc.co.za>
2024-04-03 11:00:00 +0200

    lsp update

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -13,7 +13,18 @@ return {
 		-- Set border for floating windows
 		vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers["textDocument/hover"], { border = "single" })
 		vim.lsp.handlers["textDocument/signatureHelp"] = vim.lsp.with(vim.lsp.handlers["textDocument/signatureHelp"], { border = "single" })
-		vim.diagnostic.config({ float = { border = "single" } })
+		vim.diagnostic.config({
+			float = { border = "single" },
+			underline = {
+				severity = { min = vim.diagnostic.severity.WARN },
+			},
+			virtual_text = {
+				severity = { min = vim.diagnostic.severity.ERROR },
+			},
+			signs = {
+				severity = { min = vim.diagnostic.severity.ERROR },
+			},
+		})
 
 		require("mason").setup()
 		require("mason-lspconfig").setup({
