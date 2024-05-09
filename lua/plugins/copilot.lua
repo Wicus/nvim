81b48be48e02b7f6727870318838c00a67008619
Wicus <wicus@mecalc.co.za>
2024-05-09 14:24:46 +0200

    Added copilot cmp back

--- lua/plugins/copilot.lua
+++ lua/plugins/copilot.lua
@@ -2,47 +2,55 @@ local keymaps = require("keymaps.core").keymaps
 local vim_keymap_set = require("keymaps.core").vim_keymap_set
 
 return {
-	"zbirenbaum/copilot.lua",
-	opts = {
-		panel = {
-			enabled = false,
-		},
-		suggestion = {
-			enabled = true,
-			auto_trigger = false,
-			keymap = {
-				accept = "<Tab>",
+	{
+		"zbirenbaum/copilot.lua",
+		opts = {
+			panel = {
+				enabled = false,
+			},
+			suggestion = {
+				enabled = true,
+				auto_trigger = false,
+				keymap = {
+					accept = "<Tab>",
+				},
+			},
+			filetypes = {
+				yaml = false,
+				markdown = true,
+				help = false,
+				gitcommit = false,
+				gitrebase = false,
+				hgcommit = false,
+				svn = false,
+				cvs = false,
+				["."] = false,
 			},
 		},
-		filetypes = {
-			yaml = false,
-			markdown = true,
-			help = false,
-			gitcommit = false,
-			gitrebase = false,
-			hgcommit = false,
-			svn = false,
-			cvs = false,
-			["."] = false,
-		},
+		config = function(_, opts)
+			local copilot = require("copilot")
+			local copilot_suggestion = require("copilot.suggestion")
+
+			copilot.setup(opts)
+
+			vim_keymap_set(keymaps.toggle_copilot, require("copilot.suggestion").toggle_auto_trigger)
+
+			vim.keymap.set("i", "<M-;>", function()
+				if copilot_suggestion.is_visible() then
+					copilot_suggestion.toggle_auto_trigger()
+					copilot_suggestion.dismiss()
+				else
+					copilot_suggestion.toggle_auto_trigger()
+					copilot_suggestion.next()
+				end
+			end)
+		end,
+		cond = function() return not vim.g.vscode end,
 	},
-	config = function(_, opts)
-		local copilot = require("copilot")
-		local copilot_suggestion = require("copilot.suggestion")
-
-		copilot.setup(opts)
-
-		vim_keymap_set(keymaps.toggle_copilot, require("copilot.suggestion").toggle_auto_trigger)
-
-		vim.keymap.set("i", "<M-;>", function()
-			if copilot_suggestion.is_visible() then
-				copilot_suggestion.toggle_auto_trigger()
-				copilot_suggestion.dismiss()
-			else
-				copilot_suggestion.toggle_auto_trigger()
-				copilot_suggestion.next()
-			end
-		end)
-	end,
-	cond = function() return not vim.g.vscode end,
+	{
+		"zbirenbaum/copilot-cmp",
+		config = true,
+		dependencies = { "zbirenbaum/copilot.lua" },
+		cond = function() return not vim.g.vscode end,
+	},
 }
