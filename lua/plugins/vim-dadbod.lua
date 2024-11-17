4c67015a89b21e549112a181b04f441c0b180523
Wicus Pretorius <wicus.pretorius@iotnxt.com>
2024-11-17 22:28:51 +0200

    Add vim-dadbod

---
+++ lua/plugins/vim-dadbod.lua
@@ -0,0 +1,45 @@
+return {
+	{ "tpope/vim-dadbod", cmd = "DB" },
+	{
+		"kristijanhusak/vim-dadbod-completion",
+		dependencies = "vim-dadbod",
+		ft = "sql",
+		init = function()
+			vim.api.nvim_create_autocmd("FileType", {
+				pattern = "sql",
+				callback = function()
+					local cmp = require("cmp")
+					cmp.setup.buffer({
+						sources = {
+							{ name = "vim-dadbod-completion" },
+							{ name = "buffer" },
+						},
+					})
+				end,
+			})
+		end,
+	},
+	{
+		"kristijanhusak/vim-dadbod-ui",
+		cmd = { "DBUI", "DBUIToggle", "DBUIAddConnection", "DBUIFindBuffer" },
+		dependencies = "vim-dadbod",
+		keys = {
+			{ "<leader>D", "<cmd>DBUIToggle<CR>", desc = "Toggle DBUI" },
+		},
+		init = function()
+			local data_path = vim.fn.stdpath("data")
+
+			vim.g.db_ui_auto_execute_table_helpers = 1
+			vim.g.db_ui_save_location = data_path .. "/dadbod_ui"
+			vim.g.db_ui_show_database_icon = true
+			vim.g.db_ui_tmp_query_location = data_path .. "/dadbod_ui/tmp"
+			vim.g.db_ui_use_nerd_fonts = true
+
+			-- NOTE: The default behavior of auto-execution of queries on save is disabled
+			-- this is useful when you have a big query that you don't want to run every time
+			-- you save the file running those queries can crash neovim to run use the
+			-- default keymap: <leader>S
+			vim.g.db_ui_execute_on_save = true
+		end,
+	},
+}
